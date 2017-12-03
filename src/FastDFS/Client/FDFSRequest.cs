a99903d3fd2bd43f6a1a5902b4aaf2361a052c55
caozhiyuan <caozhiyuan@suiyi.com.cn>
2017-12-03 13:58:23 +0800

    nop

--- src/FastDFS/Client/FDFSRequest.cs
+++ src/FastDFS/Client/FDFSRequest.cs
@@ -5,87 +5,87 @@ using System.Threading.Tasks;
 namespace FastDFS.Client
 {
     internal class FDFSRequest
-	{
-		private FDFSHeader _header;
+    {
+        private FDFSHeader _header;
 
-		private byte[] _body;
+        private byte[] _body;
 
-		private Connection _connection;
+        private Connection _connection;
 
-	    protected byte[] Body
-		{
-			get => this._body;
-	        set => this._body = value;
-	    }
+        protected byte[] Body
+        {
+            get => this._body;
+            set => this._body = value;
+        }
 
-	    protected Connection Connection
-		{
-			get => this._connection;
-	        set => this._connection = value;
-	    }
+        protected Connection Connection
+        {
+            get => this._connection;
+            set => this._connection = value;
+        }
 
-	    protected FDFSHeader Header
-		{
-			get => this._header;
-	        set => this._header = value;
-	    }
+        protected FDFSHeader Header
+        {
+            get => this._header;
+            set => this._header = value;
+        }
 
         protected int ConnectionType { get; set; }
-	    protected IPEndPoint EndPoint { get; set; }
+        protected IPEndPoint EndPoint { get; set; }
 
         protected FDFSRequest()
-		{
-		}
+        {
+        }
 
-		public virtual FDFSRequest GetRequest(params object[] paramList)
-		{
-			throw new NotImplementedException();
-		}
+        public virtual FDFSRequest GetRequest(params object[] paramList)
+        {
+            throw new NotImplementedException();
+        }
 
-		public virtual byte[] GetResponse()
-		{
-		    if (this.ConnectionType == 0)
-		    {
-		        this._connection = ConnectionManager.GetTrackerConnection();
-		    }
-		    else
-		    {
-		        this._connection = ConnectionManager.GetStorageConnection(EndPoint);
+        public virtual byte[] GetResponse()
+        {
+            if (this.ConnectionType == 0)
+            {
+                this._connection = ConnectionManager.GetTrackerConnection();
             }
-		    try
-		    {
-		        this._connection.Open();
-
-		        byte[] num = this._header.ToByte();
-		        _connection.SendEx(num);
-		        _connection.SendEx(this._body);
-
-		        byte[] numArray0 = new byte[10];
-		        if (_connection.ReceiveEx(numArray0) == 0)
-		        {
-		            throw new FDFSException("Init Header Exeption : Cann't Read Stream");
-		        }
-		        var length = Util.BufferToLong(numArray0, 0);
-		        var command = numArray0[8];
-		        var status = numArray0[9];
-
-		        var fDFSHeader = new FDFSHeader(length, command, status);
-		        if (fDFSHeader.Status != 0)
-		        {
-		            throw new FDFSException($"Get Response Error,Error Code:{fDFSHeader.Status}");
-		        }
-		        byte[] numArray = new byte[fDFSHeader.Length];
-		        if (fDFSHeader.Length != (long) 0)
-		        {
-		            _connection.ReceiveEx(numArray);
-		        }
-		        return numArray;
+            else
+            {
+                this._connection = ConnectionManager.GetStorageConnection(EndPoint);
             }
-		    finally
-		    {
-		        this._connection.Close();
+            try
+            {
+                this._connection.Open();
+
+                byte[] num = this._header.ToByte();
+                _connection.SendEx(num);
+                _connection.SendEx(this._body);
+
+                byte[] numArray0 = new byte[10];
+                if (_connection.ReceiveEx(numArray0) == 0)
+                {
+                    throw new FDFSException("Init Header Exeption : Cann't Read Stream");
+                }
+                var length = Util.BufferToLong(numArray0, 0);
+                var command = numArray0[8];
+                var status = numArray0[9];
+
+                var fDFSHeader = new FDFSHeader(length, command, status);
+                if (fDFSHeader.Status != 0)
+                {
+                    throw new FDFSException($"Get Response Error,Error Code:{fDFSHeader.Status}");
+                }
+                byte[] numArray = new byte[fDFSHeader.Length];
+                if (fDFSHeader.Length != (long) 0)
+                {
+                    _connection.ReceiveEx(numArray);
+                }
+                return numArray;
             }
-		}
+            finally
+            {
+                this._connection.Close();
+            }
+        }
 
         public virtual async Task<byte[]> GetResponseAsync()
         {
@@ -132,9 +132,9 @@ namespace FastDFS.Client
             }
         }
 
-	    public byte[] ToByteArray()
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public byte[] ToByteArray()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
